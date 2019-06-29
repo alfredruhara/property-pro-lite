27b54109444f5e9a74b263eef1246607c4fa9efd
chadrack ruhara <alfredruhara@gmail.com>
2019-06-29 20:54:12 +0200

    [ch-166997887] set up mocha and chai

---
+++ server/tests/demo.Spec.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+
+describe("Demo", () => {
+
+    it("it should do something" , () => {
+       let a = 3 ;
+       assert.equal(a * 2 , 6 , "failed to multiply ");
+    })
+
+})
