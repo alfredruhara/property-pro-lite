88d69d8cdc90a9df838c5c4c6959103a8d818453
chadrack ruhara <alfredruhara@gmail.com>
2019-07-01 21:11:38 +0200

    [ft-166688972] Add Tests spec update infos user and improve code coverage

--- server/tests/userSpec.js
+++ server/tests/userSpec.js
@@ -33,17 +33,28 @@ const signinCredentials = {
     password:'123456'
 }
 
+const userUpdateInfos = {
+    firstName:'andela',
+    lastName:'user',
+    phoneNumber:'89109334',
+    address:'Uganda',
+}
+const corruptOnUpdateUserInfos = {
+    firstName:'hacker',
+    lastName:'user',
+    'email': 'fake@chada.hack'
+}
+
 // const user = userDB.find(user => req.body.email === user.email);
 const routes = {
     root   : '/',
     signup : '/api/v1/user/signup',
     signin : '/api/v1/user/signin',
     auth_signup : '/api/v1/auth/signup',
-    agents : '/api/v1/user/agents'
+    agents : '/api/v1/user/agents',
+    updateInfo : '/api/v1/user/1',
+    updateInfoWithFakeUserId : '/api/v1/user/100',
 }
-
-const authToken = 'dcgcajhacsah'
-
 
 describe('Test for the user endpoint - /api/v1/user/', () => {
 
@@ -119,18 +130,31 @@ describe('Test for the user endpoint - /api/v1/user/', () => {
               chai.expect(res.body.status).to.be.equal(BAD_REQUEST_MSG);
               chai.expect(res.type).to.be.equal('application/json');
           });
-      });
+         });
 
-      it('Should not sign a user if already signed in', (done) =>{
-        chai.request(app)
-        .post(routes.signin)
-        .send(signinCredentials)
-        .end((err,res) =>{
+         it('Should not sign a user if already signed in', (done) =>{
+            chai.request(app)
+            .post(routes.signin)
+            .send(signinCredentials)
+            .end((err,res) =>{
 
-            chai.expect(res.body.status).to.be.equal(INTERNAL_SERVER_ERROR_CODE);
-            chai.expect(res.body.error).to.be.equal('data and hash arguments required');
-            done();
+                chai.expect(res.body.status).to.be.equal(INTERNAL_SERVER_ERROR_CODE);
+                chai.expect(res.body.error).to.be.equal('data and hash arguments required');
+                done();
+         });
+
+         it('Should validate signin body inputs spec', (done) => {
+            chai.request(app)
+            .post(routes.signin)
+            .send(corruptCredentials)
+            .end((err, res) => {
+                chai.expect(res.statusCode).to.be.equal(BAD_REQUEST_CODE);
+                chai.expect(res.body.status).to.be.equal(BAD_REQUEST_MSG);
+                chai.expect(res.type).to.be.equal('application/json');
+                done();
+            });
         });
+
        });
 
   
@@ -143,6 +167,46 @@ describe('Test for the user endpoint - /api/v1/user/', () => {
             .get(routes.agents)
             .end((err, res) => {
                 chai.expect(res.statusCode).to.be.equal(SUCCESS_CODE);
+                done();
+            });
+        });
+    });
+
+    describe("User signed in", () => {
+        it('Should update user information', (done) => {
+            chai.request(app)
+            .put(routes.updateInfo)
+            .send(userUpdateInfos)
+            .end((err, res) => {
+                chai.expect(res.statusCode).to.be.equal(SUCCESS_CODE);
+                chai.expect(res.body.status).to.be.equal(SUCCESS_MSG);
+                chai.expect(res.body).to.be.an('object');
+                chai.expect(res.type).to.be.equal('application/json');
+                done();
+            });
+        });
+
+        it('Should handle to do not update user information if a fake id', (done) => {
+            chai.request(app)
+            .put(routes.updateInfoWithFakeUserId)
+            .send(userUpdateInfos)
+            .end((err, res) => {
+                chai.expect(res.statusCode).to.be.equal(BAD_REQUEST_CODE);
+                chai.expect(res.body.status).to.be.equal(BAD_REQUEST_MSG);
+                chai.expect(res.body.message).to.be.equal('Unknow a user with that ID');
+                chai.expect(res.body).to.be.an('object');
+                chai.expect(res.type).to.be.equal('application/json');
+                done();
+            });
+        });
+        it('Should validate user body inputs spec', (done) => {
+            chai.request(app)
+            .put(routes.updateInfo)
+            .send(corruptOnUpdateUserInfos)
+            .end((err, res) => {
+                chai.expect(res.statusCode).to.be.equal(BAD_REQUEST_CODE);
+                chai.expect(res.body.status).to.be.equal(BAD_REQUEST_MSG);
+                chai.expect(res.type).to.be.equal('application/json');
                 done();
             });
         });
