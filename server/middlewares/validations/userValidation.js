f63b02b24f1d05c0d500e8549bd5d2245cd5a565
chadrack ruhara <alfredruhara@gmail.com>
2019-07-01 20:24:50 +0200

    [ft-166688972] Add put endpoint user update informations

--- server/middlewares/validations/userValidation.js
+++ server/middlewares/validations/userValidation.js
@@ -57,6 +57,22 @@ class UserValidation {
     }
     return Errors.joiErrorResponse(res, result.error);
   }
+
+  static updateInformations(req, res, next) {
+    const schema = Joi.object().keys({
+      firstName: Joi.string().min(3).max(25).required(),
+      lastName: Joi.string().min(3).max(25).required(),
+      phoneNumber: Joi.number().positive().required(),
+      address: Joi.string().min(5).max(30).required()
+    });
+
+    const result = Joi.validate(req.body, schema);
+
+    if (!result.error) {
+      return next();
+    }
+    return Errors.joiErrorResponse(res, result.error);
+  }
 }
 
 export default UserValidation;
